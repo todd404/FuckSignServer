1f072825b7b08f4750dae2f735a670cc16652f46
Todd404 <wutob12450@outlook.com>
2021-04-23 16:19:39 +0000

    add: GetNewPort API

--- server.js
+++ server.js
@@ -1,13 +1,69 @@
+const ObjectId = require('mongodb').ObjectId;
 var MongoClient = require('mongodb').MongoClient
 var url = "mongodb://localhost"
 var mongo;
 
-MongoClient.connect(url, (err, db) => {
-    if (err) throw err;
-    mongo = db;
+const express = require('express');
 
-    let dbo = db.db("fksign");
-    console.log(dbo.collection("data").find({
+var app = express();
+var api = express.Router();
+
+async function connectMongo()
+{
+    return new Promise((resolve, reject)=>{
+        MongoClient.connect(url, (err, db)=>{
+            if(err) reject(err);
+
+            resolve(db);
+        })
+    })
+}
+
+async function getNewPort()
+{
+    let port;
+    try {
+        mongo = await connectMongo();
+    } catch (error) {
+        console.log(error);
+    }
+    let db = mongo.db("fksign");
+    let where = {
         "_id": ObjectId("607f98048f4c06c5a6561c6b")
-    }));
+    }
+    
+    async function queryPort()
+    {
+        return new Promise((resolve, reject)=>{
+            db.collection("data").find(where).toArray((err, result)=>{
+                if(err) reject(err);
+                resolve(result[0].port);
+            })
+        })
+    }
+
+    async function updatePort(newPort)
+    {
+        return new Promise((resolve, reject)=>{
+            let update = {
+                $set: {"port": newPort}
+            }
+            db.collection("data").updateOne(where, update, (err, res)=>{
+                if(err) reject(err);
+                resolve(res);
+            })
+        })
+    }
+
+    port = await queryPort();
+    await updatePort(port + 1);
+    return port;
+}
+
+api.get('/GetNewPort', async (req, res)=>{
+    let port = await getNewPort();
+    res.send(port.toString());
 })
+
+app.use("/api", api);
+app.listen(3000, ()=>{console.log("listening...")})
