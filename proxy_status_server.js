63d470cb8e20a0584ca3afdfc239bcc44ba949a0
toddwu <wutob12450@outlook.com>
2021-10-02 17:23:26 +0800

    update

--- proxy_status_server.js
+++ proxy_status_server.js
@@ -4,16 +4,24 @@ const axios = require('axios');
 var app = express();
 var api = express.Router();
 
-async function getProxyStatus()
+async function GetOnlinePort()
 {
+    let online_proxies_port = new Array;
     let frp_status = await axios.get("http://127.0.0.1:7500/api/proxy/tcp")
-    console.log(frp_status.data.proxies)
+    let proxies = frp_status.data.proxies;
+    for(p of proxies){
+        if(p.status === "online"){
+            online_proxies_port.push(p.conf.remote_port);
+        }
+    }
+
+    return online_proxies_port;
 }
 
 api.get('/GetProxyStatus', async (req, res)=>{
-    let port = await getProxyStatus();
+    let port = await GetOnlinePort();
     res.send(port.toString());
 })
 
 app.use("/api", api);
-app.listen(3001, ()=>{console.log("listening..."); getProxyStatus()})
+app.listen(3001, ()=>{console.log("listening..."); console.log(await GetOnlinePort())})
